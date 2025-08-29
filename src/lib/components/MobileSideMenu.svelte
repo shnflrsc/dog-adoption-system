d9e289ba588ed747d1835b82cf3cdaeaad74b16f
Shane Floresca <floresca.shanegarcia@gmail.com>
2025-08-30 01:45:10 +0800

    make mobile side menu position to fixed

--- src/lib/components/MobileSideMenu.svelte
+++ src/lib/components/MobileSideMenu.svelte
@@ -6,11 +6,11 @@
 
 {#if sideMenuState.active}
 	<article
-		class="grid grid-cols-2 grid-rows-1 md:hidden h-full absolute left-0 top-0 z-50"
+		class="grid grid-cols-2 grid-rows-1 md:hidden h-full fixed left-0 top-0 z-50"
 		transition:fly={{ x: -300, easing: cubicOut }}
 	>
 		<section
-			class="w-64 bg-orange-500 shadow-lg shadow-neutral-900 flex justify-center items-center"
+			class="w-64 bg-orange-500 shadow-lg shadow-neutral-900 flex justify-center items-center z-500"
 		>
 			<nav>
 				<ul class="flex flex-col gap-y-4">
