d578525f2b8945af01a6070ef46a6ad816d7ebc6
Shane Floresca <floresca.shanegarcia@gmail.com>
2025-08-30 19:55:06 +0800

    Vertically uncenter mobile side menu nav links

--- src/lib/components/MobileSideMenu.svelte
+++ src/lib/components/MobileSideMenu.svelte
@@ -9,11 +9,9 @@
 		class="grid grid-cols-2 grid-rows-1 md:hidden h-full fixed left-0 top-0 z-50"
 		transition:fly={{ x: -300, easing: cubicOut }}
 	>
-		<section
-			class="w-64 bg-orange-500 shadow-lg shadow-neutral-900 flex justify-center items-center z-500"
-		>
+		<section class="w-64 bg-orange-500 shadow-lg shadow-neutral-900 flex justify-center z-500">
 			<nav>
-				<ul class="flex flex-col gap-y-4">
+				<ul class="flex flex-col gap-y-4 mt-8">
 					{#each navLinks as { href, name }}
 						<li>
 							<a
